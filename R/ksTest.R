e5224c79807eaeeac9dd63de0bb91e007acb241a
jasonserviss <jason.serviss@ki.se>
2018-03-29 08:38:55 +0200

    finally sucess; removing debugging from ksTest

--- R/ksTest.R
+++ R/ksTest.R
@@ -150,7 +150,6 @@ KStest <- function(
  # gene (inner; map2_dfr). Name combinations in object, reformat to a data.frame
  # and then tibble.
  res <- map(1:length(c1), function(i) {
-   message(i)
    x <- c1[[i]]
    y <- c2[[i]]
    purrr::map2_dfr(x, y, ~runKS(.x, .y), .id = "gene")
